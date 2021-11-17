f64dd1e25df43ebab258c8b7c50fc00c0f7e3d14
unknown <you@example.com>
2021-11-17 06:36:16 +0530

    Refactored: Code and test case to calculate fare for multiple rides

--- src/test/java/com/bridgelabz/cabinvoicegeneratortest/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/cabinvoicegeneratortest/InvoiceServiceTest.java
@@ -2,28 +2,32 @@ package com.bridgelabz.cabinvoicegeneratortest;
 
 import com.bridgelabz.cabinvoicegenerator.InvoiceGenerator;
 import com.bridgelabz.cabinvoicegenerator.Ride;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class InvoiceServiceTest {
+    InvoiceGenerator invoiceGenerator = null;
+    @BeforeEach
+    void setUp() {
+        invoiceGenerator = new InvoiceGenerator();
+    }
+
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare() {
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double fare = invoiceGenerator.calculateFare(2.0, 5);
         assertEquals(25.0, fare);
     }
 
     @Test
     public void givenLessDistanceOrTime_ShouldReturnMinFare() {
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double fare = invoiceGenerator.calculateFare(0.1, 1);
         assertEquals(5, fare);
     }
 
     @Test
     public void givenMultipleRides_ShouldReturnTotalFare() {
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         Ride[] rides = {new Ride(2.0, 5),
                         new Ride(0.1, 1)
                         };
